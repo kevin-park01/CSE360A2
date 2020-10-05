11ef6c3d6d0c69ca73a43d9530616883a0d458d8
Kevin <jkphyke@gmail.com>
2020-10-04 18:06:50 -0700

    Some of the methods were completed and documentation was updated.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,47 +2,57 @@ package cse360assignment02;
 
 public class AddingMachine {
 	  private int total;
+	  private String history;
 	  
 	  /**
-	   * Constructor - initializes total
+	   * Constructor - initializes total and history
 	   */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = "0";
 	  }
 	  
 	  /**
-	   * Returns 0
-	   * @return 0
+	   * Returns total variable
+	   * @return total
 	   */
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  /**
-	   * Does nothing
+	   * Adds parameter value to total variable and records in history
 	   * @param value
 	   */
 	  public void add (int value) {
+		  total += value;
+		  history += " + " + value;
 	  }
 
 	  /**
-	   * Does nothing
+	   * Subtracts parameter from total variable and records in history
 	   * @param value
 	   */
 	  public void subtract (int value) {
+		  total -= value;
+		  history += " - " + value;
 	  }
 
 	  /**
-	   * Returns an empty string
-	   * @return ""
+	   * Returns history variable
+	   * @return history
 	   */
 	  public String toString () {
-	    return "";
+	    return history;
 	  }
 
 	  /**
-	   * Does nothing
+	   * Clears memory
 	   */
 	  public void clear() {
+		  history = "0";
 	  }
-	}
+}
+
+
+
