9185be177cc31007e101bbe054c762aa8c214add
Kevin <jkphyke@gmail.com>
2020-10-04 17:46:33 -0700

    Initial code.

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	  private int total;
+	  
+	  /**
+	   * Constructor - initializes total
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * Returns 0
+	   * @return 0
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * Does nothing
+	   * @param value
+	   */
+	  public void add (int value) {
+	  }
+
+	  /**
+	   * Does nothing
+	   * @param value
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * Returns an empty string
+	   * @return ""
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * Does nothing
+	   */
+	  public void clear() {
+	  }
+	}
